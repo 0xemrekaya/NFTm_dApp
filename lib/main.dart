676edb92a0e0598318c6e134954963293be8220d
Emre Kaya <emrekaya75@outlook.com>
2023-09-06 18:42:21 +0300

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nftm_dapp/screens/nft_main_screen.dart';
 import 'package:nftm_dapp/screens/splash_page.dart';
 
+import 'firebase_options.dart';
 import 'screens/import_wallet.dart';
 import 'screens/main_screen.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   await dotenv.load(fileName: ".env");
   runApp(const ProviderScope(child: MainApp()));
 }
